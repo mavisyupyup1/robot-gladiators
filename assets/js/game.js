857cc195505ee4bdec2c19754f2d51cee2a473ec
Grace Liu <graceliu@Graces-Air.home>
2021-10-10 11:13:27 -0400

    enter and store robot's name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+// window is refering to the browser. whenever JavaScript run in a browser, window will always be present.
+// a function is a prefined action we can call or invoke (after declare it earlier in the code). alert() is a function that tells the browser to display a message
+// placing content between the parentheses is called passing an argument into a function.
+// the semicolon closes out the functions code. each piece of code, separated by semicolon is known as an expression
+
+// prompt asks for user input
+// var is a variable that stores the user input (in the browser's memory)
+var playerName = window.prompt("What is your robot's name?");
+// this creates a function named "fight". the keyword "function" declares it.
+function fight(){
+window.alert("The fight has begun!");
+}
+console.log(playerName)
+console.log("This is a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("our robot's name is " + playerName)
+// the function will not show until we call it
+fight();
