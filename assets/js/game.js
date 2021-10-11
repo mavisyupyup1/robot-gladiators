ff9a7f1ee5cbb94d0d8954e3f3fa21e0fc945f83
Grace Liu <graceliu@Graces-Air.home>
2021-10-10 22:02:45 -0400

    added enemy helth

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,8 +28,19 @@ enemyHealth = enemyHealth - playerAttack;
 console.log(
     playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining. "
 );
+//check enemy's health
+if (enemyHealth <= 0){
+    window.alert(enemyName + " has died");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left. ")
+}
 // subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that that result to update the value in the 'playerHealth' variable.
+playerHealth = playerHealth - enemyAttack;
 // log a resulting message to the console so we know that it worked. 
+console.log(
+    enemyName + " attacked " + playerName + ". " +playerName + " now has " + playerHealth + " health remaining."
+);
 };
 // the function will not show until we call it 
 fight();
