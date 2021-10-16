1afb4a2186f7c01b548e67722aba3aa53df96bf5
Grace Liu <graceliu@Graces-Air.home>
2021-10-16 10:56:38 -0400

    add localStorage for highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -43,7 +43,7 @@ var randomNumber = function (min,max) {
 }
 var fightOrSkip = function () {
     //ask player if they'd like to fight or skip using fightOrSkip function
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter \"skip\" to skip or any other key to fight.")
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter \"skip\" or \"s\" to skip or any other key to fight.");
     //Enter the conditional recursive function call here!
     //Conditional Recursive function call
     if(!promptFight){
@@ -52,7 +52,7 @@ var fightOrSkip = function () {
     }
     //if player picks "skip" confirm add then stop the loop
     promptFight = promptFight.toLowerCase();
-    if(promptFight === "skip"){
+    if(promptFight === "skip" || promptFight ==="s"){
         //confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -60,13 +60,14 @@ var fightOrSkip = function () {
         if(confirmSkip){
             window.alert(playerInfo.name + "has decided to skip this fight. Goodbye!");
             //subtract money from playMoney for skipping
-            playerInfo.playerMoney = playerInfo.money -10;
+            playerInfo.playerMoney = Math.max(0,playerInfo.money -10);
+            console.log(playerInfo.money)
             //return true if player wants to leave
             return true;
-            shop();
         }
     }
-}
+    return false;
+};
 
 var fight = function(enemy) {
     //keep track of who goes first
@@ -74,7 +75,7 @@ var fight = function(enemy) {
     if(Math.random()>0.5){
         isPlayerTurn =false;
     }
-    console.log(enemy);
+    console.log(isPlayerTurn);
     while(enemy.health > 0 && playerInfo.health > 0){
         if(isPlayerTurn){
    if(fightOrSkip()){
@@ -95,6 +96,7 @@ console.log(
         //award player money for winning
         playerInfo.money= Math.max(0, playerInfo.money+20);
         //leave while() loop since enemy is dead
+        console.log(playerInfo.money);
         break;
     } else {
         window.alert(enemy.name + " still has " + enemy.health + " health left. ");
@@ -110,7 +112,14 @@ console.log("enemy damage and playerInfo.health", damage,playerInfo.health)
 // log a resulting message to the console so we know that it worked. 
 
 console.log(
-    enemy.name + " attacked " + playerInfo.name + ". " +playerInfo.name + " now has " + playerInfo.health + " health remaining."
+    enemy.name + 
+    " attacked " + 
+    playerInfo.name + 
+    ". " + 
+    playerInfo.name + 
+    " now has " + 
+    playerInfo.health + 
+    " health remaining."
 );
 
 //check player's health
@@ -123,9 +132,7 @@ console.log(
     }
     //switch turn order for next round
     isPlayerTurn =!isPlayerTurn;
-} 
-    
-    
+    }          
 };
 
 //function to start a new game
@@ -133,6 +140,8 @@ var startGame =function () {
     //reset player stats
     playerInfo.reset();
     for(var i = 0; i < enemyInfo.length; i ++){
+        //check player stats
+        console.log(playerInfo);
         if(playerInfo.health >0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
         var pickedEnemyObj= enemyInfo[i];
@@ -173,13 +182,28 @@ var startGame =function () {
 //   fight();
 //function expression (better) var add =function(){} v.s. Declaration (function (){}).
 var endGame = function () {
-    //if player is still alive, player wins!
     if(playerInfo.health > 0){
         window.alert("Great Job! You've survive the battle! You now have a score of " + playerInfo.money + ".");
     }
     else{
         window.alert("You've lost your robot in battle. ");
     }
+
+    window.alert("The has now ended.Let's see how you did!");
+//check localStorage for high score, if it is not there use 0
+var highScore = localStorage.getItem("highscore");
+if(highScore === null){
+    highScore = 0;
+}
+//if player has more money that the high score, player has new highscore!
+if(playerInfo.money > highScore){
+    localStorage.setItem("highscore",playerInfo.money);
+    localStorage.setItem("name",playerInfo.name);
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money+"!");
+}
+else{
+    alert(playerInfo.name+ " did not beat the high score of "+ highScore+". Maybe next time!");
+}
     //ask player if they'd like to play gain
     var playAgainConfirm = window.confirm ("Would you like to play again?");
   
@@ -284,5 +308,6 @@ console.log(enemyInfo);
 console.log(enemyInfo[0]);
 console.log(enemyInfo[0].name);
 console.log(enemyInfo[0]['attack']);
+console.log(playerInfo.money);
   //start first game when page loads
   startGame();
